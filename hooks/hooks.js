6dae340678ca874ea2ce66db01e3caab0e873c6e
wonknu10 <fabrice.labbe@adfab.fr>
2017-03-23 09:34:18 +0100

    better styling button display gallery

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -2,8 +2,8 @@ exports.default = {
   afterEditorInput: (htmlString, params, abe) => {
     if(params.type === 'image') {
       htmlString = htmlString.replace(
-        /(span class=\"glyphicon glyphicon-upload\"(\r|\t|\n|.)*?<\/div>)/,
-        '$1' + '<div class="open-gallery" data-init="0"><span class="glyphicon glyphicon-picture" data-id="' + params.key + '"></span></div>'
+        /<span class="image-icon"><\/span>/,
+        '<div class="open-gallery" data-init="0"><span class="glyphicon glyphicon-picture" data-id="' + params.key + '"></span></div>'
       );
       htmlString = htmlString.replace(/(<div class=\"form-group\")/g, '$1' + 'input-image-gallery');
     }
